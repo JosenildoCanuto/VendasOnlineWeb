e250b2716ed823478ca8d2132b3ca798466a6213
Josenildo Canuto <josenildo.acanuto@gmail.com>
2023-07-24 23:17:46 -0300

    feat: insert state in login

--- src/modules/login/screens/LoginScreen.tsx
+++ src/modules/login/screens/LoginScreen.tsx
@@ -1,4 +1,5 @@
 import { Input } from 'antd';
+import { useState } from 'react';
 
 import Button from '../../../shared/buttons/button/button';
 import {
@@ -11,6 +12,21 @@ import {
 } from '../styles/loginScreen.styles';
 
 const LoginScreen = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = (event: React.ChangeEvent<HTMLInputElement>) => {
+    alert(`Usuario: ${username} Senha: ${password}`);
+  };
+
   return (
     <ContainerLoginScreen>
       <BackgroundImage src="./background.png" />
@@ -20,9 +36,20 @@ const LoginScreen = () => {
           <TitleLogin level={2} type="secondary">
             LOGIN
           </TitleLogin>
-          <Input placeholder="Usuario" style={{ marginBottom: '6px' }} />
-          <Input placeholder="Senha" />
-          <Button type="primary" margin="16px 0px 16px 0px">
+          <Input
+            placeholder="Usuario"
+            style={{ marginTop: '16px' }}
+            onChange={handleUsername}
+            value={username}
+          />
+          <Input
+            placeholder="Senha"
+            type='password'
+            style={{ marginTop: '16px' }}
+            onChange={handlePassword}
+            value={password}
+          />
+          <Button type="primary" margin="16px 0px 16px 0px" onClick={handleLogin}>
             ENTRAR
           </Button>
         </LimitedContainer>
@@ -32,3 +59,9 @@ const LoginScreen = () => {
 };
 
 export default LoginScreen;
+// style={{ marginTop: '16px' }}
+//   const  [username, setUsername] = useState('');
+
+// const handleUsername = (event: React.ChangeEvent<HTMLInputElement>) = {
+//   setUsername(event.target.value)
+// }
